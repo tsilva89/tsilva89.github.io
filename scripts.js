d7b06ffc095875034079c8627565ce28a7e079c9
Thiago Silva <thiago99@outlook.com>
2022-07-24 10:56:00 -0300

    hide mobile keyboard function

--- scripts.js
+++ scripts.js
@@ -71,3 +71,12 @@ function draw() {
         date.innerHTML = "Sorteio realizado em " + dateString;
     }
 }
+
+const hideMobileKeyboardOnReturn = (element) => {
+    element.addEventListener('keyup', (keyboardEvent) => {
+        const key = keyboardEvent.code || keyboardEvent.keyCode;
+        if (key === 'Enter' || key === 13) {
+            element.blur();
+        }
+    });
+};
